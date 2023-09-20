a5501dba146011c0f754872a7cdfdab9d2e33c48
Paul Izekor <izekorpaul0@gmail.com>
2023-09-20 12:02:10 +0100

    working on introduction page

--- get-linked/src/app/components/home/Introduction.jsx
+++ get-linked/src/app/components/home/Introduction.jsx
@@ -1,8 +1,16 @@
-'use client'
-import React from "react"
+"use client";
+import React from "react";
+import styled from "styled-components";
 
 function Introduction() {
-    return <>Introduction Page</>
+  return (
+    <Wrapper>
+        <div className="wrapper">
+        </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
 
 export default Introduction;
