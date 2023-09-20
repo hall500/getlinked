ac7cd947553e4e6f3a7007a756ccc831a0a0d338
Paul Izekor <izekorpaul0@gmail.com>
2023-09-20 11:40:29 +0100

    working on introduction page

---
+++ get-linked/src/app/components/home/Introduction.jsx
@@ -0,0 +1,8 @@
+'use client'
+import React from "react"
+
+function Introduction() {
+    return <>Introduction Page</>
+}
+
+export default Introduction;
--- get-linked/src/app/page.js
+++ get-linked/src/app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Header from './components/home/Header'
+import Introduction from './components/home/Introduction'
 
 export default function Home() {
   return (
       <div>
+        <Header />
+        <Introduction />
       </div>
   )
 }
